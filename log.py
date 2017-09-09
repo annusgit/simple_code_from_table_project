286dfbaf3de99aa7360499aac7663d8dec03157b
Annus Zulfiqar <azulfiqar.bee15seecs@seecs.edu.pk>
2017-09-09 16:59:21 +0500

    final log file

--- log.py
+++ log.py
@@ -1,16 +1,14 @@
 # Author: Annus Zulfiqar 
-# a small logger class for outputing text in python
+# a small logger function to output text in python
 
-class log(object):
-    def __init__(self):
-        pass
-    def log(self, string=None, clause='log', end='\n', cute=False):
-        if not string: 
-            print('')
-            return
-        verbose = clause+': '+string if clause is not None and clause is not '' else string
-        if cute:
-            print('\b'*len(verbose), end='', flush=True)
-            print(verbose, end='')
-            return
-        print(verbose, end=end) 
+def log(string=None, clause='log', cute=False):
+    if not string:
+        print('')
+        return
+    verbose = clause+': '+string
+    if not cute:
+        print(verbose)
+        return
+    print('\b'*len(verbose), end='', flush=True)
+    print(verbose, end='')
+
